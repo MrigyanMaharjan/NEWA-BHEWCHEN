7723dff18f3f16d00487910e1ead60345bc35ea9
Mrigyan <mrigyanmaharjan@gmail.com>
2025-02-01 12:38:09 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Newa Bhewchen | #1 Online Newari Restaurant?</title>
+</head>
+<body>
+    start
+</body>
+</html>
