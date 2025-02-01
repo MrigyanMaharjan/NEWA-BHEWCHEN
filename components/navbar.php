153ce6752a7f8118a0c739598dbc2c985f7bf61f
Mrigyan <mrigyanmaharjan@gmail.com>
2025-02-01 13:09:33 +0545

    navbar incomplete

---
+++ components/navbar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+<nav>
+    <img class="logo" src="./assets/DALL·E 2025-02-01 12.50.06 - A vibrant and eye-catching logo for a Newari food business called 'Newa Bhwechen'. The design should incorporate traditional Newar symbols, such as a .webp" alt="">
+    <section class="nav-btn">
+        <a class="button" href="">TAB</a>
+        <a class="button" href="">TAB</a>
+        <a class="button" href="">TAB</a>
+    </section>
+
+</nav>    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Newa Bhewchen | #1 Online Newari Restaurant?</title>
+    <link rel="icon" href="./assets/DALL·E 2025-02-01 12.50.06 - A vibrant and eye-catching logo for a Newari food business called 'Newa Bhwechen'. The design should incorporate traditional Newar symbols, such as a .webp">
 </head>
 <body>
-    start
+    <?php 
+    include "./components/navbar.php"
+    ?>
 </body>
 </html>
